a023241abc568a0d8322ece0b4b4c92b49a539af
mythicalbadger <jussy12.work@gmail.com>
2024-03-04 10:49:51 +0700

    feat: graph/vertex models

---
+++ src/models/graph.py
@@ -0,0 +1,36 @@
+"""The module for the Graph class, which represents a graph of vertices."""
+
+from typing import Dict, Set
+
+import networkx as nx
+
+from src.models.vertex import Vertex
+
+
+class Graph:
+    """A class to represent a graph of vertices."""
+
+    def __init__(self, vertices: Set[Vertex]) -> None:
+        """Initialize a graph with a set of vertices."""
+        self.vertices: Dict[int, Vertex] = {v.label: v for v in vertices}
+
+    def add_edge(self, v1_label: int, v2_label: int) -> None:
+        """Add an edge between two vertices."""
+        v1, v2 = self.vertices[v1_label], self.vertices[v2_label]
+        v1.add_edge(v2)
+        v2.add_edge(v1)
+
+    def remove_edge(self, v1_label: int, v2_label: int) -> None:
+        """Remove the edge between two vertices."""
+        v1, v2 = self.vertices[v1_label], self.vertices[v2_label]
+        v1.remove_edge(v2)
+        v2.remove_edge(v1)
+
+    def to_networkx(self) -> nx.Graph:
+        """Return a networkx graph representation of this graph."""
+        g = nx.Graph()
+        for v in self.vertices.values():
+            g.add_node(v.label)
+            for n in v.neighbors:
+                g.add_edge(v.label, n.label)
+        return g
---
+++ src/models/vertex.py
@@ -0,0 +1,36 @@
+"""Module containing the Vertex class, which represents a node in a graph."""
+
+from typing import Set
+
+
+class Vertex:
+    """A class to represent a vertex in a graph."""
+
+    def __init__(self, label: int) -> None:
+        """Initialize a vertex with a label and an empty set of neighbors."""
+        self.label = label
+        self.neighbors: Set[Vertex] = set()
+
+    def add_edge(self, other: "Vertex") -> None:
+        """Add an edge between this vertex and another vertex."""
+        self.neighbors.add(other)
+
+    def remove_edge(self, other: "Vertex") -> None:
+        """Remove the edge between this vertex and another vertex."""
+        self.neighbors.remove(other)
+
+    def degree(self) -> int:
+        """Return the degree of this vertex."""
+        return len(self.neighbors)
+
+    def __str__(self) -> str:
+        """Return the label of this vertex as a string."""
+        return f"Node({self.label})"
+
+    def __repr__(self) -> str:
+        """Return the label of this vertex as a string."""
+        return f"Node({self.label})"
+
+    def __hash__(self) -> int:
+        """Return the hash of this vertex."""
+        return hash(self.label)
